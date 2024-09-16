b0d77f8361aaaced791e7cd884d4b14a201120a1
xolikov <a.xolikov@newuu.uz>
2024-09-16 20:22:09 +0500

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     long long int n;
     cin >> n;
-    if (n > INT_MIN && n < INT_MAX) cout << "YES";
+    if (n > INT_MIN + 1 && n < INT_MAX) cout << "YES";
     else cout << "NO";
     return 0;
 }
