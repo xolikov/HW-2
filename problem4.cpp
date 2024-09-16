2786a721c655b78cfd3fd60908928a33e0958bcd
xolikov <a.xolikov@newuu.uz>
2024-09-16 20:11:39 +0500

    Create problem4.cpp

---
+++ problem4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    vector<int> n(4);
+    cin >> n[0] >> n[1] >> n[2] >> n[3];
+    vector<int> ignore;
+    int ans = 0;
+    for (int i = 0; i < 4; i++) {
+        int cnt = count(n.begin(), n.end(), n[i]);
+        int ignore_cnt = count(ignore.begin(), ignore.end(), n[i]);
+        if (cnt > 1 && ignore_cnt == 0) {
+            ignore.push_back(n[i]);
+            ans += cnt - 1;
+        }
+    }
+    cout << ans;
+    return 0;
+}
